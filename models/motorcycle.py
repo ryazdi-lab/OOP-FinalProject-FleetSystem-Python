66aa78bd12a25cd9bd50743bd2216c955364d5f8
Lilia-Mori <lin248499@gmail.com>
2026-06-19 19:15:39 +0330

    feat: implement Motorcycle class
    Add Motorcycle class extending Vehicle with insurance support.

---
+++ models/motorcycle.py
@@ -0,0 +1,27 @@
+from models.vehicle import Vehicle
+from models.mixins import  Insurable
+
+class Motorcycle(Vehicle, Insurable):
+    def __init__(self, plate_number: str, brand: str, year: int, base_daily_rate: float):
+        super().__init__(plate_number, brand, year, base_daily_rate)
+
+    def calculate_daily_cost(self) -> float:
+        return self.base_daily_rate * 0.9
+
+    def get_vehicle_type(self) -> str:
+        return "Motorcycle"
+
+    def perform_maintenance(self) -> None:
+        print("\nMotorcycle is checking...")
+        print("Lights -> checked!")
+        print("Brake Pads -> repaired!")
+        print("Mirrors -> checked!")
+        print("Chain tension -> adjusted!")
+        print("Tire Pressure -> checked!")
+        print("Done! The Motorcycle is good to go.")
+
+    def calculate_insurance_premium(self) -> float:
+        return self.calculate_daily_cost() * 0.2
+
+    def is_eligible_for_coverage(self) -> bool:
+        return self.year >= 2000
