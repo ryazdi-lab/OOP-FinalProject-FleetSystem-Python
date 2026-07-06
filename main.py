f18ca739d5efe58f4f92552c9cd18a5bc0d49a85
unknown <Parsa>
2026-07-06 14:55:32 +0330

    feat: complete fleet simulation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from models.vehicle import Vehicle
-
+from models.car import Car
+from models.truck import Truck
+from models.motorcycle import Motorcycle
 def run_fleet_simulation(fleet: list[Vehicle]) -> None:
     for v in fleet:
         v.display_info()
@@ -10,7 +12,8 @@ def run_fleet_simulation(fleet: list[Vehicle]) -> None:
 if __name__ == "__main__":
     # دانشجویان اینجا اشیاء concrete را می‌سازند و به لیست اضافه می‌کنند
     fleet: list[Vehicle] = [
-        # Car("12345", "Toyota", 2020, 150.0),
-        # Truck("TRK-99", "Volvo", 2018, 300.0),
-    ]
+    Car("11A111", "Toyota", 2022, 120.0, 5),
+    Truck("22B222", "Volvo", 2020, 250.0, 8),
+    Motorcycle("33C333", "Honda", 2021, 80.0, 250),
+]
     run_fleet_simulation(fleet)
