7ed71086b68e6189f458450194602e15dcd0aad4
mgtcll <soltaniimahdieh@gmail.com>
2026-07-09 15:49:45 -0700

    Update __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,14 @@
+from models.vehicle import Vehicle
+from models.mixins import Rentable, Insurable
+from models.car import Car
+from models.motorcycle import Motorcycle
+from models.truck import Truck
+
+__all__ = [
+    'Vehicle',
+    'Rentable',
+    'Insurable',
+    'Car',
+    'Motorcycle',
+    'Truck'
+]
